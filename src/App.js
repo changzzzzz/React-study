a077954669dc146b335c1cf7385204e12ecb82c0
Yang Liu <youngliu0614@gmail.com>
2024-09-04 12:52:15 +1000

    Route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import NavBar from './component/nav-bar';
 import PostList from './component/post-list';
 import Footer from './component/footer';
+import WritePost from  './component/write-post';
 import './App.scss';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 
 
 function App() {
   return (
+    <Router>
     <div className="App" data-bs-theme="dark">
       <NavBar></NavBar>
       <div className='container main-container'>
+        <Routes>
+          <Route path='/' element={<PostList></PostList>}></Route>
+          <Route path='/post-list' element={<PostList></PostList>}></Route>
+          <Route path='/write-post' element={<WritePost></WritePost>}></Route>
+        </Routes>
         <PostList></PostList>
       </div>
       <Footer></Footer>
+      
     </div>
+    </Router>
   );
 
 }
